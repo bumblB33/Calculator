86fd29e6f69d9b4c810bf97e39b3702d88215ed0
bumblb33 <bndaven77@protonmail.com>
2025-04-09 22:00:22 -0500

    Adding project files and empty test file

---
+++ Calculator/Challenge1_UnitTests.cs
@@ -0,0 +1,15 @@
+using System;
+using Challenge;
+
+namespace Challenge
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            // TODO : Write test code
+        }
+    }
+}
